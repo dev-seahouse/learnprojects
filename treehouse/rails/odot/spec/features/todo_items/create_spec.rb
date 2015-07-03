994217bfa3fa1b12e809bd7701b6d23d79a7fbc5
dev-seahouse <xinkenan.11@hotmail.com>
2015-07-03 20:02:08 +0800

    Add specs for core eiditng items

--- treehouse/rails/odot/spec/features/todo_items/create_spec.rb
+++ treehouse/rails/odot/spec/features/todo_items/create_spec.rb
@@ -11,7 +11,7 @@ describe "Adding todo items" do
 
 	 it "is sucessful with valid content" do
 	 	 visit_todo_list(todo_list)
-	 	 click_link "New Todo Items"
+	 	 click_link "New Todo Item"
 	 	 fill_in "Content", with: "Milk"
 	 	 click_button "Save"
 	 	 expect(page).to have_content("Added todo list items.")
@@ -19,5 +19,27 @@ describe "Adding todo items" do
 	 	 	expect(page).to have_content("Milk")
 	 	 end
 
+	 end
+
+	 it "displays an error with no content" do
+	 	visit_todo_list(todo_list)
+	 	click_link "New Todo Item"
+	 	fill_in "Content", with:""
+	 	click_button "Save"
+	 	within("div.flash") do
+	 		expect(page).to have_content("There was a problem adding that todo list item")
+	 	end
+	 	expect(page).to have_content("Content can't be blank")
+	 end
+
+	 it "displays an error with content less than 2 characters long" do
+	 	visit_todo_list(todo_list)
+	 	click_link "New Todo Item"
+	 	fill_in "Content", with:""
+	 	click_button "Save"
+	 	within("div.flash") do
+	 		expect(page).to have_content("There was a problem adding that todo list item")
+	 	end
+	 	expect(page).to have_content("Content is too short")
 	 end
 end
