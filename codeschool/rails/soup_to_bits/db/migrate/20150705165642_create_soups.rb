35dc06a3a6d8e4c6b14bf719366e652129252ae1
dev-seahouse <xinkenan.11@hotmail.com>
2015-07-06 03:25:29 +0800

    soup bit completed

---
+++ codeschool/rails/soup_to_bits/db/migrate/20150705165642_create_soups.rb
@@ -0,0 +1,12 @@
+class CreateSoups < ActiveRecord::Migration
+  def change
+    create_table :soups do |t|
+      t.string :name
+      t.boolean :featured
+      t.string :category_id
+      t.string :integer
+
+      t.timestamps null: false
+    end
+  end
+end
