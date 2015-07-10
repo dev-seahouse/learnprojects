a4fd903f0058f4af752675077ecb223525c109c0
dev-seahouse <xinkenan.11@hotmail.com>
2015-07-11 01:53:35 +0800

    Add genres through BoookGenres

--- codeschool/rails/soup_to_bits2/app/models/book.rb
+++ codeschool/rails/soup_to_bits2/app/models/book.rb
@@ -1,9 +1,14 @@
 class Book < ActiveRecord::Base
+	has_many :book_genres
+	has_many :genres, through: :book_genres
 
 	scope :finished , ->{where('finished_on IS NOT NULL')}
 	scope :recent , ->{where('finished_on > ?',2.days.ago)}
 	#scope :search, ->(keyword){ where(title: keyword) if keyword.present?}
-	scope :search, ->(keyword){ where('keywords LIKE?',"%#{keyword.downcase}%")}
+	scope :search, ->(keyword){where('keywords LIKE?', "%#{keyword.downcase}%") if keyword.present? }
+	#scope :filter, ->(genre_id){ joins(:book_genres).where(book_genres:{genre_id: genre_id}) if genre_id.present?}
+	scope :filter, ->(name){ joins(:genres).where('genres.name=?',name) if name.present?}
+	#where('book_genres.genre_id=?',genre_id)
 	# % means wild card
 	before_save :set_keywords
 
