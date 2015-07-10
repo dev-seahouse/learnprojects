09aec5b816632119c0f547206aac1363fa95ce6a
dev-seahouse <xinkenan.11@hotmail.com>
2015-07-10 18:01:38 +0800

    Initial search. Title only

--- codeschool/rails/soup_to_bits2/app/models/book.rb
+++ codeschool/rails/soup_to_bits2/app/models/book.rb
@@ -1,4 +1,15 @@
 class Book < ActiveRecord::Base
+
+	scope :finished , ->{where('finished_on IS NOT NULL')}
+	scope :recent , ->{where('finished_on > ?',2.days.ago)}
+	scope :search, ->(keyword){ where(title: keyword) if keyword.present?}
+	# def self.search(keyword)
+	# 	if (keyword.present?)
+	# 		where(title: keyword)
+	# 	else
+	# 		all
+	# 	end
+	# end
 	def finished?
 		finished_on.present?
 	end
