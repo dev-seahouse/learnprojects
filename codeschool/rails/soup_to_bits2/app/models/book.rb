cdb87918d3b0d3ea93a6b6d8984b74551abef731
dev-seahouse <xinkenan.11@hotmail.com>
2015-07-10 19:00:06 +0800

    Improve search with keywords.

--- codeschool/rails/soup_to_bits2/app/models/book.rb
+++ codeschool/rails/soup_to_bits2/app/models/book.rb
@@ -2,7 +2,13 @@ class Book < ActiveRecord::Base
 
 	scope :finished , ->{where('finished_on IS NOT NULL')}
 	scope :recent , ->{where('finished_on > ?',2.days.ago)}
-	scope :search, ->(keyword){ where(title: keyword) if keyword.present?}
+	#scope :search, ->(keyword){ where(title: keyword) if keyword.present?}
+	scope :search, ->(keyword){ where('keywords LIKE?',"%#{keyword.downcase}%")}
+	# % means wild card
+	before_save :set_keywords
+
+
+
 	# def self.search(keyword)
 	# 	if (keyword.present?)
 	# 		where(title: keyword)
@@ -13,4 +19,11 @@ class Book < ActiveRecord::Base
 	def finished?
 		finished_on.present?
 	end
+
+	protected
+		def set_keywords
+			#self.keywords = [title,author, description].map{|p| p.downcase}
+			self.keywords = [title,author,description].map(&:downcase).join(' ')
+			#go through each item in the array and set downcase to each of them
+		end
 end
