09431532cde3e383a4355a097c9df78cb11d5f8d
Kenan <xinkenan.11@hotmail.com>
2016-05-13 01:59:09 +0800

    changed

--- middleman/sites/mysite/gulpfile.js
+++ middleman/sites/mysite/gulpfile.js
@@ -1,48 +1,46 @@
 var gulp = require('gulp');
-var livereload = require('gulp-livereload')
+var livereload = require('gulp-livereload');
 var uglify = require('gulp-uglifyjs');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-
+var wait = require('gulp-wait');
 
 
 
 gulp.task('imagemin', function () {
-    return gulp.src('./wp-content/themes/olympos/images/*')
+    return gulp.src('./source/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./wp-content/themes/olympos/images'));
+        .pipe(gulp.dest('./source/images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./wp-content/themes/olympos/sass/**/*.scss')
+  gulp.src('./source/stylesheets/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./wp-content/themes/olympos'));
+    .pipe(gulp.dest('./source/stylesheets'));
 });
 
-
-gulp.task('uglify', function() {
-  gulp.src('./wp-content/themes/olympos/lib/*.js')
-    .pipe(uglify('olympos.min.js'))
-    .pipe(gulp.dest('./wp-content/themes/olympos/js'))
-});
+// gulp.task('uglify', function() {
+//   gulp.src('./wp-content/themes/olympos/lib/*.js')
+//     .pipe(uglify('olympos.min.js'))
+//     .pipe(gulp.dest('./wp-content/themes/olympos/js'))
+// });
 
 gulp.task('watch', function(){
     livereload.listen();
 
-    gulp.watch('./wp-content/themes/olympos/sass/**/*.scss', ['sass']);
-    gulp.watch('./wp-content/themes/olympos/lib/*.js', ['uglify']);
-    gulp.watch(['./wp-content/themes/olympos/style.css', './wp-content/themes/olympos/*.php', './wp-content/themes/olympos/js/*.js', './wp-content/themes/olympos/parts/**/*.php'], function (files){
+    gulp.watch('./source/stylesheets/**/*.scss', ['sass']);
+    gulp.watch(['./source/stylesheets/style.css','./source/javascripts/*.js', './source/**/*.erb'], function (files){
         livereload.changed(files)
     });
 });
