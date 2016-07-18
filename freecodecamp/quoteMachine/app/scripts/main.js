b354388685b262dbaa68fbdeb2a106e9347d7aa5
kenan <xinkenan.11@hotmail.com>
2016-07-18 19:45:26 +0800

    update

--- freecodecamp/quoteMachine/app/scripts/main.js
+++ freecodecamp/quoteMachine/app/scripts/main.js
@@ -1,65 +1,49 @@
 "use strict";
-var $jsonp = (function () {
-    var jsonpRequest = function (url, options) {
-        return new jsonpRequest.init(url, options);
-    };
-
-    jsonpRequest.prototype = {
-        configureTimeOut: function () {
-            var ops = this.options;
-
-            var timeout_trigger = window.setTimeout(function () {
-                window[ops.callback_name] = function () { };
-                ops.onTimeout();
-            }, ops.timeout * 1000);
-
-            window[ops.callback_name] = function (data) {
-                window.clearTimeout(timeout_trigger);
-                ops.onSucess(data);
-            };
-        },
-
-        createScript : function () {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.async = true;
-            script.src = this.url;
-            document.getElementsByTagName('head')[0].appendChild(script);
-        },
-        configureOptions: function () {
-            for (var prop in default_options) {
-                if (!this.options[prop]) {
-                    this.options[prop] = default_options[prop];
+(function(global){
+    'use strict';
+    
+    var apiUrl = 'https://api.unsplash.com/photos/random?collections=',
+        apiKey = 'af5a74750e1773ca6657744a27653509508e04bc1f1c1610f7d0e2bc7b526ad0',
+        separator='&';
+        
+    // constructor
+    var RandomPhoto = function(collectionId){
+        this.collectionId = collectionId || '156576,159165';
+        this.url = this.getNext();
+    }
+    
+    // get the url of the next random photo
+    RandomPhoto.prototype.getNext = function(){
+        var xhttp = new XMLHttpRequest(),
+            url = apiUrl + this.collectionId + separator + "client_id="+ apiKey;
+            
+        xhttp.onreadystatechange = function(){
+            if (xhttp.readyState == 4 && xhttp.status == 200){
+                var res = JSON.parse(xhttp.responseText);
+                if (res) {
+                    console.log(res.urls.full);
+                    return res.urls.full;
+                }else {
+                    throw "Empty response";
                 }
             }
-        }
+        }; 
+        
+        xhttp.open('GET', url, true);
+        xhttp.send();
     };
+    global.RandomPhoto = RandomPhoto;   
+}(window));
+var $view = $('#view');
+var photo = new RandomPhoto();
+// var imgUrl = "https://source.unsplash.com/collection/156576?";
+// var changeBg = function(){
+//     imgUrl+=new Date().getTime();
+//     $view.css('background-image',imgUrl);
+// }
+// window.setInterval(changeBg,5000);
 
-    var default_options = {
-        callback_name: 'callback',
-        onSucess: function () {
-            console.log("Success");
-        },
-        onTimeout: function () {
-            console.log("Timeout");
-        },
-        timeout: 10
-    };
 
-    jsonpRequest.init = function (url, options) {
-        if (!url) {
-            throw "jsonp request url not specified";
-        }
-        this.url = url;
-        this.options = options || default_options;
-        this.configureOptions();
-        this.configureTimeOut();
-        this.createScript();
-    };
 
-    jsonpRequest.init.prototype = jsonpRequest.prototype;
 
-    return jsonpRequest;
-
-} ());
 
